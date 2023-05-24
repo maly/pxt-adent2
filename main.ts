21c54b6034c46f5a7b88d2485de6173023074959
Martin Malý <maly@maly.cz>
2023-05-24 23:19:24 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+//% color="#AA278D"
+//% groups="['Fruits', 'Veggies']"
 namespace adent2 {
+    //% block
+    //% group="Veggies"
     export const soucet = (a:number,b:number) => a+b;
+    //% block
+    //% group="Fruits"
+    export const rozdil = (a: number, b: number) => a + b;
 }
